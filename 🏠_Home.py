30bbb9cfd35a5f9f4c7418e4466b35d8a07f0232
Mamadou Ndiaye <redgisafrica@gmail.com>
2022-07-24 22:13:24 +0000

    change

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -57,26 +57,14 @@ contact_form = """
 """  
 st.markdown(contact_form, unsafe_allow_html=True)
 
+
 # Use Local CSS
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 local_css("style/style.css")
 
-# Documentation: https://formsubmit.co/ !!!CHANGE EMAIL ADRESS !!!
-contact_form = """
-<form action="https://formsubmit.co/mamadou10.ndiaye87@gmail.com" method="POST">
-    <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-    <textarea name="message" rows="10" placeholder="Your Message Here" required></textarea> 
-    <button type="submit">Send</button> 
-    
-</form>
-"""       
-st.markdown(contact_form, unsafe_allow_html=True)
-
-    
+   
         
     
             
