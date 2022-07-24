306a1e17d81342a5a020ab389ac387800c8eff90
Mamadou Ndiaye <redgisafrica@gmail.com>
2022-07-24 19:43:00 +0000

    Email change for contact form

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -44,7 +44,7 @@ st.header(":mailbox: Get In Touch With Me!")
 
 
 contact_form = """
-<form action="https://formsubmit.co/mamadou10.ndiaye87@gmail.com" method="POST">
+<form action="https://formsubmit.co/redgisafrica@gmail.com" method="POST">
     <input type="hidden" name="_captcha" value="false">
      <input type="text" name="name" placeholder="Your name" required>
      <input type="email" name="email" placeholder="Your email" required>
