c28bbf0c5353c36ebc6a6a141c778204e1dde007
Mamadou Ndiaye <redgisafrica@gmail.com>
2022-07-24 19:27:52 +0000

    added form contact

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -38,3 +38,26 @@ st.markdown(markdown)
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
 m.to_streamlit(height=500)
+
+
+st.header(":mailbox: Get In Touch With Me!")
+
+
+contact_form = """
+<form action="https://formsubmit.co/mamadou10.ndiaye87@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+    <textarea name="message" rows="10" placeholder="Your Message Here" required></textarea> 
+    <button type="submit">Send</button> 
+    
+</form>
+"""  
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
+
